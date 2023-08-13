7a9bdf88d46e79d818c4cc010f7379c187571e41
Dom <domnguyen531@gmail.com>
2023-08-13 13:28:37 -0500

    Add icons to projects in Apps page
    - Change padding and margins to clean up UI with new icons

--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -2,34 +2,44 @@
 </script>
 
 <div class="flex flex-col h-full w-full justify-center items-center">
-	<div class="flex flex-col h-screen w-full justify-center items-center overflow-hidden bg-zinc-900">
+	<div
+		class="flex flex-col h-screen w-full justify-center items-center overflow-hidden bg-zinc-900 bg-gradient-to-b from-zinc-900 to-zinc-950"
+	>
 		<div class="absolute top-10 lg:top-20">
-            <div class="max-w-lg p-4">
-                <h1 class="text-4xl font-bold mb-1">
-                    Dom's apps
-                </h1>
-                <p>Here are a few projects I'm currently building:</p>
-            </div>
-			
+			<div class="max-w-lg p-4">
+				<h1 class="text-4xl font-bold mb-3">Dom's apps</h1>
+				<p>Here are a few projects I'm currently building:</p>
+			</div>
 
-			<div class="mt-5 p-4">
-				<a class="card max-w-lg hover:bg-zinc-950" href="https://xplorepod.com" target="_blank">
-					<h2 class="text-2xl font-bold underline">XplorePod</h2>
-					<p>
-						A space-themed app that makes it fun to focus, track, and do more of what matters to you
-						most.
-					</p>
+			<div class="mt-5">
+				<a class="card max-w-lg hover:bg-zinc-950 hover:animate-pulse p-4 flex flex-row items-center justify-center" href="https://xplorepod.com" target="_blank">
+                    <img src="scout-icon.svg" alt="xplorepod icon" class="ml-1 mr-3 h-16"/>
+                    <div>
+                        <h2 class="text-2xl font-bold underline">XplorePod</h2>
+                        <p>
+                            A space-themed app that makes it fun to focus, track, and do more of what matters to you
+                            most.
+                        </p>
+                    </div>
+		
 				</a>
 				<a
-					class="card max-w-lg hover:bg-zinc-950 mt-3"
+					class="card max-w-lg hover:bg-zinc-950 hover:animate-pulse p-4 flex flex-row items-center mt-3"
 					href="https://rovee.app"
 					target="_blank"
 				>
+                <img src="rovee.svg" alt="xplorepod icon" class="ml-1 mr-3 h-16"/>
+                <div>
 					<h2 class="text-2xl font-bold underline">ROVEE</h2>
-					<p>A fun simple app that helps you stop scrolling on social media in the morning.</p>
+					<p>A fun space-cleaning app that helps you stop scrolling on social media in the morning.</p>
+                </div>
 				</a>
 
-                <p class="mt-10">Learn more about me <a href="/" class="underline hover:opacity-75">here</a>.</p>
+				<div class="p-4">
+					<p class="mt-5">
+						Learn more about me <a href="/" class="underline hover:opacity-75">here</a>.
+					</p>
+				</div>
 			</div>
 		</div>
 
