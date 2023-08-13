08d662c9acbe915c4b9359a64fa668d8296bf65b
Dom <domnguyen531@gmail.com>
2023-08-13 00:26:30 -0500

    Add minor UI polishes to Apps page
    - Add tundra planet on bottom
    - And link to main page
    - Add underline class to project names to show they are clickable

--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
 </script>
 
-<div class="h-screen flex flex-col justify-center items-center bg-zinc-900">
-    
-	<h1 class="text-4xl font-bold mb-1">Apps</h1>
-	<p>Here are a few of my current projects:</p>
+<div class="flex flex-col h-full w-full justify-center items-center">
+	<div class="flex flex-col h-screen w-full justify-center items-center overflow-hidden bg-zinc-900">
+		<div class="absolute top-10 lg:top-20">
+            <div class="max-w-lg p-4">
+                <h1 class="text-4xl font-bold mb-1">
+                    Dom's apps
+                </h1>
+                <p>Here are a few projects I'm currently building:</p>
+            </div>
+			
 
-	<div class="mt-10">
-		<a class="card max-w-lg p-4 hover:bg-zinc-950" href="https://xplorepod.com" target="_blank">
-			<h2 class="text-2xl font-bold">XplorePod</h2>
-			<p>
-				A space-themed app that makes it fun to focus, track, and do more of what matters to you
-				most.
-			</p>
-		</a>
-		<a class="card max-w-lg p-4 hover:bg-zinc-950 mt-1" href="https://rovee.app" target="_blank">
-			<h2 class="text-2xl font-bold">ROVEE</h2>
-			<p>A fun simple app that helps you stop scrolling on social media in the morning.</p>
-        </a>
+			<div class="mt-5 p-4">
+				<a class="card max-w-lg hover:bg-zinc-950" href="https://xplorepod.com" target="_blank">
+					<h2 class="text-2xl font-bold underline">XplorePod</h2>
+					<p>
+						A space-themed app that makes it fun to focus, track, and do more of what matters to you
+						most.
+					</p>
+				</a>
+				<a
+					class="card max-w-lg hover:bg-zinc-950 mt-3"
+					href="https://rovee.app"
+					target="_blank"
+				>
+					<h2 class="text-2xl font-bold underline">ROVEE</h2>
+					<p>A fun simple app that helps you stop scrolling on social media in the morning.</p>
+				</a>
+
+                <p class="mt-10">Learn more about me <a href="/" class="underline hover:opacity-75">here</a>.</p>
+			</div>
+		</div>
+
+		<div class="-mb-[50rem] scale-[2] z-0">
+			<img src="tundra-I.svg" alt="tundra planet" />
+		</div>
 	</div>
 </div>
