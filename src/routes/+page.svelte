c01582c925a286f4453b2458e0aa9e9057ed1122
Dom <domnguyen531@gmail.com>
2025-09-16 20:35:34 -0500

    Remove Key Areas section for now and update Personal Mission

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -95,7 +95,17 @@
 			desc: 'Visual storytelling of menu analytics startup using animations at NMotion 2018 demo day',
 			link: 'https://www.youtube.com/watch?v=N7g0P_zAIEo',
 			date: 'Dec 2018',
-			tags: ['Keynote', 'Angular', 'Typescript', 'Firebase', 'NodeJS', 'PWA', 'RxJS', 'Ionic', 'Capacitor']
+			tags: [
+				'Keynote',
+				'Angular',
+				'Typescript',
+				'Firebase',
+				'NodeJS',
+				'PWA',
+				'RxJS',
+				'Ionic',
+				'Capacitor'
+			]
 		},
 		{
 			title: 'Refyne',
@@ -180,11 +190,11 @@
 			<h1 class="text-3xl font-bold animate-pulse hiddenBottom">Personal Mission</h1>
 
 			<p
-				class="text-4xl lg:text-5xl font-extrabold py-6 max-w-md md:max-w-xl text-gradient hiddenLeft"
+				class="text-4xl lg:text-5xl font-extrabold py-6 max-w-md md:max-w-lg text-gradient hiddenLeft"
 			>
-				I aim to <span class="text-white">empower others to use technology for good</span>
-				by unlocking their potential to
-				<span class="text-[#DD7BEC] text-glow-change">create positive change.</span>
+				I aim to <span class="text-white">build experiences</span>
+				that make life
+				<span class="text-[#DD7BEC] text-glow-change">more meaningful.</span>
 			</p>
 		</div>
 		<img
@@ -195,104 +205,10 @@
 	</div>
 </div>
 
-<!-- Key Areas -->
-<div
-	class="hero min-h-screen bg-svg bg-no-repeat bg-center"
-	style="background-image: url('/bg-personal-key-areas.svg')"
->
-	<div class="hero-content flex-col lg:flex-row mb-5">
-		<div class="md:pl-8">
-			<h1 class="text-3xl font-bold text-white">Key Areas</h1>
-			<p class="text-pd mb-2">
-				Below are areas I think are key to achieving my personal mission. I'm aware I might not have
-				have time to work on all of these, so if one appeals to you, go for it! I truly believe
-				these are needed in the world so it honestly shouldn't matter who gets the credit for it.
-			</p>
-			<div class="mb-5 rounded-3xl area">
-				<h2 class="text-2xl font-bold text-primary text-glow-primary hiddenBottom">
-					<Icon data={powerOff} />
-					Area 1: Authentic Personal Focus
-				</h2>
-				<p class="text-pd hiddenLeft">
-					Focus is necessary for people to prioritize what's personally important and maximize their
-					potential and impact on the world. To help, I highly recommend apps like
-					<Link name="Forest" link="https://www.forestapp.cc/" color="primary" /> and
-					<Link name="Fabulous" link="https://www.thefabulous.co/" color="primary" />
-					which aid in digital detox and building healthy habits.
-					<br /><br />
-					However, there's still an opportunity to specifically help those who want to create good yet
-					lack focus or discipline, something I personally recently struggled with. That's why I'm building
-					<Link name="XplorePod" link="https://xplorepod.com" color="primary" /> and <Link
-						name="ROVEE"
-						link="https://rovee.app"
-						color="primary"
-					/> to address this -
-					<i>sign up today!</i>
-				</p>
-			</div>
-			<div class="mb-5 rounded-3xl area">
-				<h2 class="text-2xl font-bold text-accent text-glow-accent hiddenBottom">
-					<Icon data={book} />
-					Area 2: Enhanced STEM Education
-				</h2>
-				<p class="text-pd hiddenLeft">
-					STEM education is crucial to prepare people to accelerate positive societal change.
-					<Link name="Brilliant" link="https://brilliant.org/" /> excels in affordable interactive content
-					yet could benefit from greater breadth and industry recognition. Meanwhile,
-					<Link name="Udacity" link="https://www.udacity.com/" /> leads in professional development but
-					is quite expensive.
-					<br /><br />
-					Thus, I believe there are still ways to make STEM learning more interactive and accessible.
-					<i>I'm actively looking for opportunities to contribute in this space.</i>
-				</p>
-			</div>
-			<div class="mb-10 rounded-3xl area">
-				<h2 class="text-2xl font-bold text-secondary text-glow-secondary hiddenBottom">
-					<Icon data={group} />
-					Area 3: Purpose-led Recruiting
-				</h2>
-				<p class="text-pd hiddenLeft">
-					Matching purpose-driven individuals with organizations that share their values is
-					essential for reducing turnover and driving positive change.
-					<Link name="Idealist" link="https://www.idealist.org/" color="secondary" /> and
-					<Link name="MovingWorlds" link="https://movingworlds.org/" color="secondary" /> offer valuable
-					resources in the nonprofit sector, while <Link
-						name="LinkedIn"
-						link="https://www.linkedin.com/"
-						color="secondary"
-					/> is an excellent tool for professional networking.
-					<br /><br />
-					Nevertheless, I think there's a potential for for-profit organizations to make purpose-driven
-					recruiting the norm rather than an exception.
-					<i>I plan to build something in this area soon!</i>
-				</p>
-			</div>
-			<div class="mb-10 rounded-3xl area">
-				<h2 class="text-2xl font-bold text-success text-glow-success hiddenBottom">
-					<Icon data={group} />
-					Area 4: Tech-Driven Greener Energy
-				</h2>
-				<p class="text-pd hiddenLeft">
-					None of the above endeavors matter if we don't preserve our planet. Having worked several
-					years at <Link name="ExxonMobil" link="https://exxonmobil.com" color="success" />, I've
-					learned how vital energy is to power modern everyday life. Yet, digital innovation and new
-					technologies should be leveraged to make energy more sustainable and not only to meet
-					demand.
-					<br /><br />
-					The transition to cleaner energy needs to happen at several layers, both through reducing emissions
-					in established industries such as oil & gas and developing new technologies and systems where
-					alternative energy sources can thrive.
-					<i>I'd love to find ways to contribute in this space again.</i>
-				</p>
-			</div>
-		</div>
-	</div>
-</div>
-
 <!-- Design Portfolio -->
 <div
 	class="hero min-h-screen bg-svg bg-no-repeat bg-center"
-	style="background-image: url('/bg-personal-design-portfolio.svg')"
+	style="background-image: url('/bg-personal-key-areas.svg')"
 >
 	<div class="hero-content flex-col lg:flex-row mb-5">
 		<div class="md:pl-8 mt-10">
